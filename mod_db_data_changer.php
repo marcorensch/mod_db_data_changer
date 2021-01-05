040c02de16db07fbb4982aefae46b19bc97786ae
marcorensch <marco.rensch@nx-designs.ch>
2021-01-05 09:56:21 +0100

    Version 1.0.6

--- mod_db_data_changer.php
+++ mod_db_data_changer.php
@@ -14,13 +14,17 @@ defined('_JEXEC') or die;
 
 require_once __DIR__ . '/helper.php';
 
-if(array_key_exists('sectoken', $_GET) && htmlspecialchars($_GET['sectoken']) === htmlspecialchars($params->get('token',''))){
+use Joomla\CMS\Factory;
+$input = Factory::getApplication()->input;
+$sectoken = $input->get('sectoken', '', 'string');
+
+if(strlen($sectoken) && $sectoken === htmlspecialchars($params->get('token',''))){
     if($params->get('debug',1)) echo 'Security Token accepted!';
     // Security Token is fine so lets do it
     $responses = ModDbDataChanger::prepareTask($params);
 }else{
     if($params->get('debug',1)) {
-        if (!array_key_exists('sectoken', $_GET) || array_key_exists('sectoken', $_GET) && !strlen(htmlspecialchars($_GET['sectoken']))) {
+        if (!strlen($sectoken)) {
             echo '<p class="alert alert-warning">Security Token not given!</p>';
         } else {
             echo '<p class="alert alert-danger">Security Token is wrong! - Check DB data changer module with ID: '.$module->id.' "'.$module->title.'"</p>';
@@ -28,9 +32,6 @@ if(array_key_exists('sectoken', $_GET) && htmlspecialchars($_GET['sectoken']) ==
         echo '<div class="alert alert-message">Hint: To hide this message you have to deactivate the debug mode of the module '.$module->title.'.</div>';
     }
 }
-
-
-
 
 
 // The below line is no longer used in Joomla 4
